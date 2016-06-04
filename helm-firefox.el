396edfa0329ce65e41bd3b28399ed5dcb023a103
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2016-06-04 09:52:49 +0200

    Remove commentary in helm-firefox.el

--- helm-firefox.el
+++ helm-firefox.el
@@ -19,16 +19,6 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-;;; Commentary:
-;;
-;; You will have to set firefox to import bookmarks in his html file bookmarks.html.
-;; (only for firefox versions >=3)
-;; To achieve that, open about:config in firefox and double click on this line to enable value
-;; to true:
-;; user_pref("browser.bookmarks.autoExportHTML", false);
-;; You should have now:
-;; user_pref("browser.bookmarks.autoExportHTML", true);
-;; NOTE: This is also working in the same way for mozilla aka seamonkey.
 
 ;;; Code:
 (require 'cl-lib)
