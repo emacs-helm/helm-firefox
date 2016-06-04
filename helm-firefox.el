38fa62ad9895bf31c29f419be5788c632f54511a
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2016-06-04 08:57:21 +0200

    Remove the show structure stuff.

--- helm-firefox.el
+++ helm-firefox.el
@@ -48,16 +48,8 @@ On Mac OS X, probably set to \"~/Library/Application Support/Firefox/\"."
   :group 'helm-firefox
   :type 'string)
 
-(defcustom helm-firefox-show-structure nil
-  "Show the directory structure of bookmark when non-nil."
-  :group 'helm-firefox
-  :type 'boolean)
-
 (defvar helm-firefox-bookmark-url-regexp "\\(https\\|http\\|ftp\\|about\\|file\\)://[^ \"]*")
 (defvar helm-firefox-bookmarks-regexp ">\\([^><]+.\\)</[aA]>")
-(defvar helm-firefox-bookmarks-subdirectory-regex "<H[1-6][^>]*>\\([^<]*\\)</H.>")
-(defvar helm-firefox-separator " » ")
-
 
 (defface helm-firefox-title
     '((t (:inherit 'font-lock-type-face)))
@@ -77,42 +69,6 @@ On Mac OS X, probably set to \"~/Library/Application Support/Firefox/\"."
               (kill-buffer)))))
     (file-name-as-directory (concat moz-dir moz-user-dir))))
 
-(defun helm-firefox-bookmarks-to-alist (file url-regexp bmk-regexp)
-  "Parse html bookmark FILE and return an alist with (title . url) as elements."
-  (let (bookmarks-alist url title stack)
-    (with-temp-buffer
-      (insert-file-contents file)
-      (goto-char (point-min))
-      (while (re-search-forward "href=\\|^ *<DT><A HREF=\\|<DT><H\\|</DL>" nil t)
-        (forward-line 0)
-        (cond (;; Matches on bookmark folders (<H3>...</H3>).
-               (string-equal (match-string 0) "<DT><H")
-               ;; Extract bookmark folders name
-               (if (re-search-forward
-                    helm-firefox-bookmarks-subdirectory-regex
-                    (point-at-eol) t)
-                   (push (match-string 1) stack)))
-              (;; Matches end of bookmark folder.
-               (string-equal (match-string 0) "</DL>")
-               (pop stack))
-              (t
-               (when (re-search-forward url-regexp nil t)
-                 (setq url (match-string 0)))
-               (when (re-search-forward bmk-regexp nil t)
-                 (setq title (funcall helm-html-decode-entities-function (match-string 1))))
-               (push
-                (cons
-                 ;; "Dir >> Dir >> Title"
-                 (if helm-firefox-show-structure
-                     (mapconcat 'identity
-                                (reverse (cons title stack))
-                                helm-firefox-separator)
-                     title)
-                 url)
-                bookmarks-alist)))
-        (forward-line)))
-    (nreverse bookmarks-alist)))
-
 (defun helm-guess-firefox-bookmark-file ()
   "Return the path of the Firefox bookmarks file."
   (expand-file-name "bookmarks.html" (helm-get-firefox-user-init-dir)))
@@ -121,14 +77,11 @@ On Mac OS X, probably set to \"~/Library/Application Support/Firefox/\"."
 (defvar helm-source-firefox-bookmarks
   (helm-build-sync-source "Firefox Bookmarks"
     :init (lambda ()
-            (let ((parser (if helm-firefox-show-structure
-                              #'helm-firefox-bookmarks-to-alist
-                              #'helm-html-bookmarks-to-alist)))
-              (setq helm-firefox-bookmarks-alist
-                    (funcall parser
-                             (helm-guess-firefox-bookmark-file)
-                             helm-firefox-bookmark-url-regexp
-                             helm-firefox-bookmarks-regexp))))
+            (setq helm-firefox-bookmarks-alist
+                  (helm-html-bookmarks-to-alist
+                   (helm-guess-firefox-bookmark-file)
+                   helm-firefox-bookmark-url-regexp
+                   helm-firefox-bookmarks-regexp)))
     :candidates (lambda ()
                   (cl-loop for (bmk . url) in helm-firefox-bookmarks-alist
                            collect (concat bmk "\n" url)))
@@ -148,7 +101,7 @@ On Mac OS X, probably set to \"~/Library/Application Support/Firefox/\"."
 (defun helm-firefox-bookmarks-get-value (elm)
   (assoc-default elm helm-firefox-bookmarks-alist))
 
-(defun helm-firefox-highlight-bookmarks-1 (bookmarks)
+(defun helm-firefox-highlight-bookmarks (bookmarks _source)
   (cl-loop for bmk in bookmarks
            for split = (split-string bmk "\n")
            collect (cons (concat (propertize
@@ -157,29 +110,6 @@ On Mac OS X, probably set to \"~/Library/Application Support/Firefox/\"."
                                  "\n"
                                  (cadr split))
                          (cadr split))))
-
-(defun helm-firefox-highlight-bookmarks-2 (bookmarks)
-  (cl-loop for i in bookmarks
-           for elements = (split-string i helm-firefox-separator)
-           for path = (butlast elements)
-           for prefix = (if path
-                            (concat
-                             (mapconcat 'identity path helm-firefox-separator)
-                             helm-firefox-separator)
-                          "")
-           for title = (car (last elements))
-           collect (concat
-                    (propertize
-                     prefix 'face '((:foreground "DarkGray"))
-                     'help-echo (helm-firefox-bookmarks-get-value i))
-                    (propertize
-                     title 'face '((:foreground "YellowGreen"))
-                     'help-echo (helm-firefox-bookmarks-get-value i)))))
-
-(defun helm-firefox-highlight-bookmarks (bookmarks _source)
-  (if helm-firefox-show-structure
-      (helm-firefox-highlight-bookmarks-2 bookmarks)
-      (helm-firefox-highlight-bookmarks-1 bookmarks)))
 
 ;;;###autoload
 (defun helm-firefox-bookmarks ()
